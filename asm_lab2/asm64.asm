8979ff0ca737dae6f923c53c827d46eb0d3b1484
K-D-Ax021 <steampphelp@gmail.com>
2024-10-05 14:47:45 +1000

    Add project files.

---
+++ asm_lab2/asm64.asm
@@ -0,0 +1,19 @@
+.data
+start_number byte 11001110b
+.code
+asm_func PROC
+	mov al, start_number
+
+	mov cl, 8 ; счетчик для цикла на 8 итераций
+	xor bl, bl
+	reverse_loop:
+		rcr al, 1	; после сдвига вправо, бит который сдвинули сохраняется в cf
+		rcl bl, 1	; передвигаем сохраненвй бит из cf влево
+		loop reverse_loop
+		mov al, bl
+
+
+	ret	
+asm_func ENDP
+
+end
